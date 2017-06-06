87008c4fdeea1612279ffc977ca99991d28de609
Shiho <hiyoko311@excite.co.jp>
2017-06-06 22:08:20 +0900

    Update: Cloud SQL Proxy

--- app_v2/install.sh
+++ app_v2/install.sh
@@ -6,6 +6,7 @@ mkdir -p /opt/cloudsqlproxy/
 pushd $(dirname $0)
 cp -R ./* /opt/dengonban/v2/
 cp ./dengonban.service /etc/systemd/system/
+cp ./cloudsqlproxy.service /etc/systemd/system/
 popd
 
 chmod 700 /opt/dengonban/v2/app.py
